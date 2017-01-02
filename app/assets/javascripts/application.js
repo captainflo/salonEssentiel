b6b7ab6b666a6312b97ffeb3663def3774437845
florian lahitte <lahitte.florian@gmail.com>
2017-01-02 17:08:00 +0100

    Add footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,32 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$(document).on('turbolinks:load', function() {
+
+  // activer les liens internes, précédé par un #
+  $('a[href^="#"]').click(function(event) {
+    event.preventDefault();
+    // prendre le nom du lien, substr enleve le premier caractere qui est #
+    var ou = $(this).attr("href").substr(1);
+    // trouver le tag <a name=""> correspondant
+    var saut = $("#"+ ou);
+    // aller jusqu'à cet élément
+    $('html, body').animate({
+        scrollTop: saut.offset().top
+    }, 1000);
+  });
+
+  $('[data-spy="scroll"]').each(function () {
+  var $spy = $(this).scrollspy('refresh')
+  })
+
+  $(window).scroll(function() { // check if scroll event happened
+        if ($(document).scrollTop() > 400) { // check if user scrolled more than 50 from top of the browser window
+          $(".navbar-wagon").css("background-color", "#f8f8f8"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+        } else {
+          $(".navbar-wagon").css("background-color", "transparent"); // if not, change it back to transparent
+        }
+      });
+
+});

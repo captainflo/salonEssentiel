53f698df6eeb83d4eaaec490c75acfa19651633a
florian lahitte <lahitte.florian@gmail.com>
2017-01-05 17:25:53 +0100

    modif some text and link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,7 +35,7 @@ $(document).on('turbolinks:load', function() {
   $('[data-spy="scroll"]').each(function () {
   var $spy = $(this).scrollspy('refresh')
   })
-
+  //for change navbar color
   $(window).scroll(function() { // check if scroll event happened
         if ($(document).scrollTop() > 400) { // check if user scrolled more than 50 from top of the browser window
           $(".navbar-wagon").css("background-color", "#f8f8f8"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
@@ -43,12 +43,8 @@ $(document).on('turbolinks:load', function() {
           $(".navbar-wagon").css("background-color", "transparent"); // if not, change it back to transparent
         }
       });
-
+      //for animation move right and left
       window.sr = ScrollReveal({ reset: true });
-
-    // Customizing a reveal set
       sr.reveal('.mov_left', { duration: 1000, origin: 'left', mobile: true });
       sr.reveal('.mov_right', { duration: 1000, origin: 'right', mobile: true });
-
-
 });
